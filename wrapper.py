0fc3a08f51fd52c88208063cfe76ca27341d888c
Lukas Hannen <git.c@hannen.at>
2025-09-19 02:15:11 +0200

    initial commit

---
+++ wrapper.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""
+goimapnotify wrapper.
+
+necessary because goimapnotify keeps crashing when no network is available,
+and does not restart endlessly.
+"""
+
+import signal
+import subprocess
+import sys
+from time import sleep
+
+class TermHandler:
+    def __init__(self, proc):
+        self.proc = proc
+        signal.signal(signal.SIGTERM, self.terminate)
+        signal.signal(signal.SIGINT, self.terminate)
+
+    def terminate(self, signum, frame):
+        _, _ = signum, frame
+        self.proc.terminate()
+        exit(0)
+
+def main():
+    while True:
+        proc = subprocess.Popen(["goimapnotify"], text=True, stderr=subprocess.PIPE)
+        TermHandler(proc)
+        for line in proc.stderr:
+            sys.stderr.write(line)
+            result = proc.poll()
+            if result is not None:
+                if "cannot make IMAP client" not in line:
+                    exit(result)
+        sys.stderr.write("wrapper: goimapnotify died for lack of network, restarting")
+        sleep(60)
+
+if __name__ == "__main__":
+    main()
